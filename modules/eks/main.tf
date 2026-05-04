8c7ee72ffa82daf068d0d2009291958b1074037d
Buddi Nikhilesh <buddinikhilesh@gmail.com>
2026-05-04 13:02:50 -0400

    Create main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,24 @@
+# EKS Cluster Module
+# Provisions production-grade EKS cluster with managed node groups
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_node_count" {
+  type    = number
+  default = 3
+}
